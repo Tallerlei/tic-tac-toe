fa40879bd9dd9effd400ee072f0c4ea74082ff06
Tallerlei <mtallarek@gmail.com>
2021-03-15 11:25:43 +0100

    / start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+class Field {
+  marked: 'x' | 'o' | '';
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic-tac-toe';
+  fields: Field[] = new Array(9);
+
+  constructor() {
+
+  }
 }
